989db46ff1f3bbea4c3f057dbc16f4d1b3cf0448
Tobias Koch <kochtobi@users.noreply.github.com>
2024-05-24 10:02:31 +0200

    Remove dangling sentence

--- src/main/java/life/qbic/registration/openbis/DataSetProvenance.java
+++ src/main/java/life/qbic/registration/openbis/DataSetProvenance.java
@@ -7,8 +7,6 @@ import java.util.Objects;
 
 /**
  * Class holding provenance information provided by the user. This is a record of immutable data.
- * <p>
- * The provenance file contains information about the
  *
  * @since 1.0.0
  */
