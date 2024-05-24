e77ad6823676642459b6900cef53f3f852131afb
Tobias Koch <kochtobi@users.noreply.github.com>
2024-05-24 09:55:20 +0200

    rename test

--- src/test/java/life/qbic/registration/openbis/ProvenanceParserTest.groovy
+++ src/test/java/life/qbic/registration/openbis/ProvenanceParserTest.groovy
@@ -2,27 +2,24 @@ package life.qbic.registration.openbis
 
 import spock.lang.Specification
 
-/**
- * TODO!
- * <b>short description</b>
- *
- * <p>detailed description</p>
- *
- * @since <version tag>
- */
 class ProvenanceParserTest extends Specification {
 
     final File validFile = new File(ProvenanceParserTest.class.getClassLoader().getResource("valid-provenance.json").toURI());
 
-    def "parsing just works"() {
+    def "parsing a valid file works"() {
         when:
         var resultingProvenanceObject = new ProvenanceParser().parseProvenanceJson(validFile)
         then:
         resultingProvenanceObject.measurementId() == "NGSQTEST001AE-1234512312"
+        and:
         resultingProvenanceObject.origin() == "/Users/myuser/registration"
+        and:
         resultingProvenanceObject.datasetFiles() == ["file1.fastq.gz", "file2.fastq.gz"]
+        and:
         resultingProvenanceObject.user() == "/Users/myuser"
+        and:
         resultingProvenanceObject.taskId() == "ce36775e-0d06-471e-baa7-1e3b63de871f"
+        and:
         resultingProvenanceObject.history() == ["/some/dir", "/some/other/dir"]
     }
 }
